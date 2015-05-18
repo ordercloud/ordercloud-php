dd65ac5a6256051ff51a44dc49b0fc9625894e84
Michael Nel <michael.a.nel1@gmail.com>
2015-05-18 11:02:10 +0200

    Added Laravel CommandHandlerTranslator

---
+++ src/Support/CommandBus/IlluminateCommandHandlerTranslator.php
@@ -0,0 +1,35 @@
+<?php namespace Ordercloud\Support\CommandBus;
+
+use Illuminate\Container\Container;
+use ReflectionClass;
+
+class IlluminateCommandHandlerTranslator implements CommandHandlerTransalator
+{
+    /** @var Container */
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Command $command
+     *
+     * @return CommandHandler
+     *
+     * @throws CommandHandlerNotFoundException
+     */
+    public function resolve(Command $command)
+    {
+        $commandClass = new ReflectionClass($command);
+
+        $handlerClass = sprintf('%s\\Handlers\\%sHandler', $commandClass->getNamespaceName(), $commandClass->getShortName());
+
+        if ( ! class_exists($handlerClass)) {
+            throw new CommandHandlerNotFoundException($command, $handlerClass);
+        }
+
+        return $this->container->make($handlerClass);
+    }
+}
