4842ae0079863463c6d8a4c7628ebfbd1312311d
Michael Nel <michael.a.nel1@gmail.com>
2015-05-19 16:28:06 +0200

    Added new Collection & PaginatedCollection

---
+++ src/Support/Collection.php
@@ -0,0 +1,111 @@
+<?php namespace Ordercloud\Support;
+
+use ArrayAccess;
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
+use JsonSerializable;
+use Traversable;
+
+class Collection implements ArrayAccess, Countable, IteratorAggregate, JsonSerializable
+{
+    /** @var array */
+    protected $items;
+
+    function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * @return array
+     */
+    public function all()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Determine if an item exists at an offset.
+     *
+     * @param mixed $key
+     *
+     * @return bool
+     */
+    public function offsetExists($key)
+    {
+        return array_key_exists($key, $this->items);
+    }
+
+    /**
+     * Get an item at a given offset.
+     *
+     * @param mixed $key
+     *
+     * @return mixed
+     */
+    public function offsetGet($key)
+    {
+        return $this->items[$key];
+    }
+
+    /**
+     * Set the item at a given offset.
+     *
+     * @param mixed $key
+     * @param mixed $value
+     *
+     * @return void
+     */
+    public function offsetSet($key, $value)
+    {
+        if (is_null($key)) {
+            $this->items[] = $value;
+        }
+        else {
+            $this->items[$key] = $value;
+        }
+    }
+
+    /**
+     * Unset the item at a given offset.
+     *
+     * @param string $key
+     *
+     * @return void
+     */
+    public function offsetUnset($key)
+    {
+        unset($this->items[$key]);
+    }
+
+    /**
+     * Count elements of an object
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    /**
+     * Retrieve an external iterator
+     *
+     * @return Traversable An instance of ArrayIterator
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->items);
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     *
+     * @return array
+     */
+    function jsonSerialize()
+    {
+        return $this->items;
+    }
+}
---
+++ src/Support/PaginatedCollection.php
@@ -0,0 +1,76 @@
+<?php namespace Ordercloud\Support;
+
+class PaginatedCollection extends Collection
+{
+    /** @var int */
+    protected $totalCount;
+    /** @var int */
+    protected $currentPage;
+    /** @var int */
+    protected $pageSize;
+    /** @var int */
+    protected $totalNrPages;
+    /** @var int */
+    protected $nextPage;
+    /** @var int */
+    protected $previousPage;
+
+    function __construct(array $items, $totalCount, $currentPage, $pageSize)
+    {
+        parent::__construct($items);
+        $this->totalCount = $totalCount;
+        $this->currentPage = $currentPage;
+        $this->pageSize = $pageSize;
+        $this->totalNrPages = ceil($totalCount / $pageSize);
+        $this->nextPage = $this->totalNrPages > $currentPage ? $currentPage + 1 : false;
+        $this->previousPage = $currentPage > 1 ? $currentPage - 1 : false;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalCount()
+    {
+        return $this->totalCount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrentPage()
+    {
+        return $this->currentPage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPageSize()
+    {
+        return $this->pageSize;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalNrPages()
+    {
+        return $this->totalNrPages;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNextPage()
+    {
+        return $this->nextPage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPreviousPage()
+    {
+        return $this->previousPage;
+    }
+}
