59e764e259b10d3d4cfeff9154bacf54cf53dc28
Michael Nel <michael.a.nel1@gmail.com>
2015-05-19 16:17:57 +0200

    Added order statuses

--- src/Entities/Orders/OrderStatus.php
+++ src/Entities/Orders/OrderStatus.php
@@ -2,6 +2,19 @@
 
 class OrderStatus
 {
+    const STATUS_NEW = 1;
+    const STATUS_PENDING = 2;
+    const STATUS_ACCEPTED = 3;
+    const STATUS_CANCELLED = 4;
+    const STATUS_READY = 5;
+    const STATUS_COLLECTED = 6;
+    const STATUS_DELIVERED = 7;
+    const STATUS_COMPLETED = 8;
+    const STATUS_REJECTED = 9;
+    const STATUS_FLAGGED = 10;
+    const STATUS_REPLACED = 11;
+    const STATUS_PICKED_UP = 12;
+
     /** @var integer */
     private $id;
     /** @var string */
