b1c8f849c93ffca64b62b27ecc0137277c1dcbf2
Michael Nel <michael.a.nel1@gmail.com>
2015-06-11 15:38:28 +0200

    Moved request exceptions into namespace & added UnauthorizedRequestException

--- src/Requests/Exceptions/OrdercloudRequestException.php
+++ src/Requests/Exceptions/OrdercloudRequestException.php
@@ -1,6 +1,7 @@
-<?php namespace Ordercloud\Requests;
+<?php namespace Ordercloud\Requests\Exceptions;
 
 use Ordercloud\OrdercloudException;
+use Ordercloud\Requests\OrdercloudRequest;
 use Ordercloud\Support\Http\OrdercloudHttpException;
 
 class OrdercloudRequestException extends OrdercloudException
@@ -19,6 +20,22 @@ class OrdercloudRequestException extends OrdercloudException
         parent::__construct($httpException->getMessage(), $httpException->getCode());
         $this->request = $request;
         $this->httpException = $httpException;
+    }
+
+    /**
+     * @param OrdercloudRequest       $request
+     * @param OrdercloudHttpException $httpException
+     *
+     * @return UnauthorizedRequestException|static
+     */
+    public static function create(OrdercloudRequest $request, OrdercloudHttpException $httpException)
+    {
+        switch ($httpException->getCode()) {
+            case 401:
+                return new UnauthorizedRequestException($request, $httpException);
+            default:
+                return new static($request, $httpException);
+        }
     }
 
     /**
