2cab33233f4b7b9118361cb23165b67d1a52ff89
Michael Nel <michael.a.nel1@gmail.com>
2015-07-27 15:10:33 +0200

    Removed duplication on UserAddress & OrganisationAddress by introducing an Address entity

--- src/Entities/Organisations/OrganisationAddress.php
+++ src/Entities/Organisations/OrganisationAddress.php
@@ -1,78 +1,30 @@
 <?php namespace Ordercloud\Entities\Organisations;
 
-class OrganisationAddress
+use Ordercloud\Entities\Delivery\Address;
+
+class OrganisationAddress extends Address
 {
-    /** @var int */
-    private $id;
-    /** @var string */
-    private $longitude;
-    /** @var string */
-    private $latitude;
-    /** @var string */
-    private $name;
     /** @var string */
     private $description;
-    /** @var string */
-    private $streetNumber;
-    /** @var string */
-    private $streetName;
-    /** @var string */
-    private $complex;
-    /** @var string */
-    private $suburb;
-    /** @var string */
-    private $city;
-    /** @var string */
-    private $postalCode;
     /** @var int */
     private $distance;
 
     public function __construct($id, $longitude, $latitude, $name, $description, $streetNumber, $streetName, $complex, $suburb, $city, $postalCode, $distance)
     {
-        $this->id = $id;
-        $this->longitude = $longitude;
-        $this->latitude = $latitude;
-        $this->name = $name;
-        $this->description = $description;
-        $this->streetNumber = $streetNumber;
-        $this->streetName = $streetName;
-        $this->complex = $complex;
-        $this->suburb = $suburb;
-        $this->city = $city;
-        $this->postalCode = $postalCode;
+        parent::__construct(
+            $id,
+            $longitude,
+            $latitude,
+            $name,
+            $streetNumber,
+            $streetName,
+            $complex,
+            $suburb,
+            $city,
+            $postalCode
+        );
         $this->distance = $distance;
-    }
-
-    /**
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLongitude()
-    {
-        return $this->longitude;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLatitude()
-    {
-        return $this->latitude;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
+        $this->description = $description;
     }
 
     /**
@@ -81,54 +33,6 @@ class OrganisationAddress
     public function getDescription()
     {
         return $this->description;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStreetNumber()
-    {
-        return $this->streetNumber;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStreetName()
-    {
-        return $this->streetName;
-    }
-
-    /**
-     * @return string
-     */
-    public function getComplex()
-    {
-        return $this->complex;
-    }
-
-    /**
-     * @return string
-     */
-    public function getSuburb()
-    {
-        return $this->suburb;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCity()
-    {
-        return $this->city;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPostalCode()
-    {
-        return $this->postalCode;
     }
 
     /**
