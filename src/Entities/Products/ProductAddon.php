825a1dc09ae10066e691b54ec79aec9436bbd22f
Michael Nel <michael.a.nel1@gmail.com>
2015-08-05 16:40:45 +0200

    Removed organisation from ProductAddon

--- src/Entities/Products/ProductAddon.php
+++ src/Entities/Products/ProductAddon.php
@@ -6,14 +6,11 @@ abstract class ProductAddon extends AbstractProductAddon
 {
     /** @var boolean */
     private $enabled;
-    /** @var OrganisationShort */
-    private $organisation;
 
-    public function __construct($id, $name, $description, $price, $enabled, OrganisationShort $organisation)
+    public function __construct($id, $name, $description, $price, $enabled)
     {
         parent::__construct($id, $name, $description, $price);
         $this->enabled = $enabled;
-        $this->organisation = $organisation;
     }
 
     /**
@@ -22,13 +19,5 @@ abstract class ProductAddon extends AbstractProductAddon
     public function isEnabled()
     {
         return $this->enabled;
-    }
-
-    /**
-     * @return OrganisationShort
-     */
-    public function getOrganisation()
-    {
-        return $this->organisation;
     }
 }
