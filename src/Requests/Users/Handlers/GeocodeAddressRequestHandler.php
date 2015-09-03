476753f2f8e468397954f5e5aa77300bd0d964ae
Michael Nel <michael.a.nel1@gmail.com>
2015-09-03 13:25:13 +0200

    Tabs :(

--- src/Requests/Users/Handlers/GeocodeAddressRequestHandler.php
+++ src/Requests/Users/Handlers/GeocodeAddressRequestHandler.php
@@ -5,20 +5,20 @@ use Ordercloud\Requests\Users\GeocodeAddressRequest;
 
 class GeocodeAddressRequestHandler extends AbstractPutRequestHandler
 {
-	/**
-	 * @param GeocodeAddressRequest $request
-	 */
-	protected function configure($request)
-	{
-		$this->setUrl('resource/users/geocode')
-			->setParameters([
-				'streetName'   => $request->getStreetName(),
-				'streetNumber' => $request->getStreetNumber(),
-				'city'         => $request->getCity(),
-				'suburb'       => $request->getSuburb(),
-				'complex'      => $request->getComplex(),
-				'postalCode'   => $request->getPostalCode(),
-			])
-			->setEntityClass('Ordercloud\Entities\Users\UserAddress');
-	}
+    /**
+     * @param GeocodeAddressRequest $request
+     */
+    protected function configure($request)
+    {
+        $this->setUrl('resource/users/geocode')
+            ->setParameters([
+                'streetName'   => $request->getStreetName(),
+                'streetNumber' => $request->getStreetNumber(),
+                'city'         => $request->getCity(),
+                'suburb'       => $request->getSuburb(),
+                'complex'      => $request->getComplex(),
+                'postalCode'   => $request->getPostalCode(),
+            ])
+            ->setEntityClass('Ordercloud\Entities\Users\UserAddress');
+    }
 }
