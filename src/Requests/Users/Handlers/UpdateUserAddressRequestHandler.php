685a8b93cc9b4d31cfebc79dfad562fa509f3d27
Michael Nel <michael.a.nel1@gmail.com>
2015-09-07 11:41:40 +0200

    Removed commented code

--- src/Requests/Users/Handlers/UpdateUserAddressRequestHandler.php
+++ src/Requests/Users/Handlers/UpdateUserAddressRequestHandler.php
@@ -31,6 +31,5 @@ class UpdateUserAddressRequestHandler extends AbstractPutRequestHandler
     protected function transformResponse($response)
     {
         return $response;
-//        return EntityReflector::parseResourceIDFromURL($response->getUrl());
     }
 }
