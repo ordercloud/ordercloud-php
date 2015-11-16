b3d267377d99f21c5706681a2b8d1775732cbae0
Michael Nel <michael.a.nel1@gmail.com>
2015-11-16 17:05:46 +0200

    Updated payment service docs

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -1,13 +1,17 @@
 <?php namespace Ordercloud\Services;
 
+use Ordercloud\Requests\Exceptions\OrdercloudRequestException;
 use Ordercloud\Requests\Payments\CreateCashOnDeliveryPaymentRequest;
 use Ordercloud\Requests\Payments\CreateCreditCardPaymentRequest;
 use Ordercloud\Requests\Payments\Entities\CreditCard;
+use Ordercloud\Requests\Payments\Exceptions\CreditCardPaymentFailedException;
 
 class PaymentService extends OrdercloudService
 {
     /**
      * @param int $orderId
+     *
+     * @throws OrdercloudRequestException
      */
     public function createCashOnDeliveryPayment($orderId)
     {
@@ -21,6 +25,9 @@ class PaymentService extends OrdercloudService
      * @param CreditCard $card
      * @param int        $orderID
      * @param string     $budgetPeriod
+     *
+     * @throws CreditCardPaymentFailedException
+     * @throws OrdercloudRequestException
      */
     public function createCreditCardPayment($paymentGateway, CreditCard $card, $orderID, $budgetPeriod = '0')
     {
