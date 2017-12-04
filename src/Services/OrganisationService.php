c4a0a1e1be59fcecae216b1431e50fa09c8ed701
Wessel Pieterse <wessel@ordercloud.co.za>
2017-12-04 16:13:52 +0200

    updated the name of the function

--- src/Services/OrganisationService.php
+++ src/Services/OrganisationService.php
@@ -173,7 +173,7 @@ class OrganisationService extends OrdercloudService
      * @param OrganisationProfile $profile
      * @return mixed
      */
-    public function create($name, $code, OrganisationProfile $profile)
+    public function createOrganisation($name, $code, OrganisationProfile $profile)
     {
         return $this->request(new CreatenOrganisationRequest($name, $code,  $profile));
     }
